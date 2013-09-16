b40de07a90e185f27b6d83bd2a26f26b49ac7840
Bruno Arueira <contato@brunoarueira.com>
2013-09-16 18:26:19 -0300

    Customized css and js to improve the layout

--- assets/themes/custom/js/custom.js
+++ assets/themes/custom/js/custom.js
@@ -4,11 +4,24 @@ $(function() {
     event.preventDefault();
     event.stopPropagation();
 
-    var elementClick = $(this).attr("href"),
+    var self = $(this),
+        elementClick = self.attr("href"),
         destination = $(elementClick).offset().top - 60;
 
     if (elementClick == "#home") {
       destination = 0;
+    }
+
+    if (!self.hasClass('to-top')) {
+      $.each(self.parent().parent().children('li'), function(index, item) {
+        var itemElementClick = $(item).children('a').attr('href');
+
+        if (elementClick !== itemElementClick) {
+          $(item).removeClass('active');
+        }
+      });
+
+      self.parent().toggleClass('active');
     }
 
     $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1100);
